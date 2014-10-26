191b8acf79456ffda0331a4654ee037e37c5b837
Scott Barrett <scott.barrett@mymail.champlain.edu>
2014-10-25 23:03:08 -0400

    Fixed some Fatty constants, I think? Need to compare them with previous commits.

--- src/game/zombies/zombietypes/Fatty.java
+++ src/game/zombies/zombietypes/Fatty.java
@@ -10,10 +10,10 @@ import java.awt.geom.Point2D;
 
 public class Fatty extends Zombie {
 
-	private final static int SIZE = 7;
-	private final static int HEALTH = 100;
-	private final static double SPEED = 2;
-	private final static int MUNNEY = 5;
+	private final static int SIZE = 10;
+	private final static int HEALTH = 200;
+	private final static double SPEED = 1.5;
+	private final static int MUNNEY = 10;
 	private final static Color COLOR = Color.YELLOW;
 	
 	public Fatty(Room parent, Path path, Point2D pos) {
